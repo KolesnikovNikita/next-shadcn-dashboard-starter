3c744d47f6713af56a3c38ac391e38993122352b
Mike <kolesnikov.nikita.andrew@gmail.com>
2025-03-06 11:46:18 +0200

    style login page

--- src/features/auth/components/user-auth-form.tsx
+++ src/features/auth/components/user-auth-form.tsx
@@ -5,18 +5,18 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import { useTransition } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import * as z from 'zod';
-import GithubSignInButton from './github-auth-button';
 
 const formSchema = z.object({
   email: z.string().email({ message: 'Enter a valid email address' })
@@ -28,13 +28,20 @@ export default function UserAuthForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
   const [loading, startTransition] = useTransition();
-  const defaultValues = {
-    email: 'demo@gmail.com'
-  };
+
   const form = useForm<UserFormValue>({
-    resolver: zodResolver(formSchema),
-    defaultValues
+    resolver: zodResolver(formSchema)
   });
+
+  const { register, handleSubmit, setValue } = useForm();
+
+  const [isRole, setRole] = useState(true);
+
+  const toggleRole = () => {
+    setRole((prev) => !prev);
+    setValue('role', isRole ? 'player' : 'agent');
+    console.log(isRole);
+  };
 
   const onSubmit = async (data: UserFormValue) => {
     startTransition(() => {
@@ -49,45 +56,83 @@ export default function UserAuthForm() {
   return (
     <>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className='w-full space-y-2'
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
           <FormField
             control={form.control}
             name='email'
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
                 <FormControl>
                   <Input
-                    type='email'
-                    placeholder='Enter your email...'
-                    disabled={loading}
+                    className='mb-6'
+                    type='text'
+                    placeholder='Player Username'
                     {...field}
                   />
                 </FormControl>
+                <div className='mb-6'>
+                  <label className='relative flex h-12 w-full cursor-pointer items-center rounded-full border bg-white shadow-sm'>
+                    <span
+                      className={`flex-1 text-center font-bold transition ${
+                        isRole ? 'text-green-900 underline' : 'text-gray-400'
+                      }`}
+                    >
+                      AGENT
+                    </span>
+
+                    <Input
+                      type='checkbox'
+                      checked={!isRole}
+                      onChange={toggleRole}
+                      className='sr-only'
+                    />
+
+                    <input
+                      type='hidden'
+                      {...register('role')}
+                      value={isRole ? 'agent' : 'player'}
+                    />
+
+                    <div
+                      className={`flex h-12 w-16 items-center justify-center rounded-full bg-green-900 transition-all duration-300 ${
+                        isRole ? 'bg-yellow-300' : 'bg-green-300'
+                      }`}
+                    ></div>
+                    <span
+                      className={`flex-1 text-center font-bold transition ${
+                        !isRole ? 'text-yellow-500 underline' : 'text-gray-400'
+                      }`}
+                    >
+                      PLAYER
+                    </span>
+                  </label>
+                </div>
                 <FormMessage />
               </FormItem>
             )}
           />
 
-          <Button disabled={loading} className='ml-auto w-full' type='submit'>
-            Continue With Email
+          <Button
+            className='mb-6 ml-auto mt-6 h-12 w-full bg-blue-500 text-base font-semibold text-white'
+            type='submit'
+          >
+            Register
           </Button>
         </form>
       </Form>
       <div className='relative'>
-        <div className='absolute inset-0 flex items-center'>
-          <span className='w-full border-t' />
-        </div>
         <div className='relative flex justify-center text-xs uppercase'>
           <span className='bg-background px-2 text-muted-foreground'>
-            Or continue with
+            Already have an account?{' '}
+            <Link
+              href='/#'
+              className='underline underline-offset-4 hover:text-primary'
+            >
+              Sign in
+            </Link>
           </span>
         </div>
       </div>
-      <GithubSignInButton />
     </>
   );
 }
