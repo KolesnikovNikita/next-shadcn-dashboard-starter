9f002e8291190125c3223525c44943765cb2728f
Mike <kolesnikov.nikita.andrew@gmail.com>
2025-03-31 17:26:56 +0300

    response api token

--- src/app/authentication/linkupverify/page.tsx
+++ src/app/authentication/linkupverify/page.tsx
@@ -2,18 +2,58 @@
 
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { saveToken } from '@/lib/auth';
+
+interface TokenResponse {
+  accessToken: string;
+  refreshToken: string;
+}
 
 export default function LinkUpVerify() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
-  console.log('token', token);
+  console.log('router', router);
+  console.log('searchParams', searchParams);
 
   useEffect(() => {
-    // Сразу редиректим на email верификацию
-    router.replace('/verification/email');
-  }, [router]);
+    const sendTokenToApi = async (token: string) => {
+      try {
+        const response = await fetch(
+          `https://mngapi.azurewebsites.net/api/Account/login-access-token?token=${token}`,
+          {
+            method: 'POST',
+            headers: {
+              Accept: 'text/plain'
+            },
+            body: JSON.stringify({ token })
+          }
+        );
+
+        const data: TokenResponse = await response.json();
+        const { accessToken, refreshToken } = data;
+
+        // console.log('accessToken', accessToken);
+        // console.log('refreshToken', refreshToken);
+        if (!response.ok) {
+          throw new Error('Failed to send token');
+        }
+
+        saveToken(token);
+        router.replace('/verification/email');
+      } catch (error) {
+        console.error('Error sending token:', error);
+        router.replace('/');
+      }
+    };
+
+    if (token) {
+      sendTokenToApi(token);
+    } else {
+      router.replace('/');
+    }
+  }, [token, router]);
 
   return (
     <div className='flex min-h-screen items-center justify-center'>
--- src/app/verification/phone/page.tsx
+++ src/app/verification/phone/page.tsx
@@ -131,7 +131,7 @@ export default function PhoneVerification() {
         </Form>
       </div>
 
-      {isCodeSent && <OtpInput />}
+      {isCodeSent && <OtpInput onVerify={verifyCode} />}
     </>
   );
 }
